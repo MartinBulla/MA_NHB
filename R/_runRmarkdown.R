18ac4139ce6f0c39eb7958996ab3e811d4f2bd6a
Martin Bulla <MartinBulla@users.noreply.github.com>
2026-04-29 09:48:22 +0200

    Prepare v2.0.0 release

--- R/_runRmarkdown.R
+++ R/_runRmarkdown.R
@@ -1,6 +1,7 @@
 require(rmarkdown)
 
-rmarkdown::render('R/rep_SI_v1.R', output_dir = 'Output', output_file = 'SI_v1.html') #, quiet = TRUE)
+rmarkdown::render('R/rep_SI_v1.R', output_dir = 'Output', output_file = 'SI_v2c.html') #, quiet = TRUE)
 
-rmarkdown::render('R/test.R', output_dir = 'Output',  output_file = 'test.html')#, quiet = TRUE)
+rmarkdown ::render('R/space_test.R', output_dir = 'Output',  output_file = 'space_test.html')#, quiet = TRUE)
 
+#' <hr style="margin: 1.5em 0 2em 0;">
